5b2f836dae962618f1cdedfd5f8bf7812e531a98
tolis-0 <yotravik@gmail.com>
2024-07-07 18:14:48 +0300

    added backwards pass for batch inputs

--- nn.c
+++ nn.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <cblas.h>
 #include "nn.h"
 
 #ifndef __x86_64__
@@ -374,5 +375,48 @@ void nn_batch_forward_pass(nn_struct_t *nn, int batch_size)
     }
 
     nn->output = nn->batch_outputs[nn->n_layers - 1];
+}
+
+
+/*  Applies backpropagation and optimizes the neural network */
+void nn_batch_backward_pass(nn_struct_t *nn, int batch_size)
+{
+    int i;
+    value_t *swap_ptr;
+
+    for (i = nn->n_layers - 1; i >= 0; i--) {
+        switch(nn->op_types[i]) {
+            case NO_OP: break;
+            case DENSE_OP:;
+                dim3_t d = {nn->n_dims[i-1], nn->n_dims[i], batch_size};
+                batch_dense_backward(d, nn->batch_outputs[i-1], nn->weights[i],
+                    i != 0, nn->ones, nn->g_out, nn->g_in, nn->g_w,
+                    nn->n_biases[i] > 0, nn->g_b);
+
+                /* TODO: MOVE TO ANOTHER FILE AND ADD REGULARIZATION */
+                cblas_daxpy(nn->n_weights[i], -nn->learning_rate,
+                    nn->g_w, 1, nn->weights[i], 1);
+
+                if (nn->n_biases[i] > 0) {
+                    cblas_daxpy(nn->n_biases[i], -nn->learning_rate,
+                        nn->g_b, 1, nn->biases[i], 1);
+                }
+                /* .......................... */
+                break;
+            case RELU_OP:
+                relu_backward(nn->n_dims[i] * batch_size,
+                    nn->batch_outputs[i-1], nn->g_out, nn->g_in);
+                break;
+            case LOGISTIC_OP:
+                logistic_backward(nn->n_dims[i] * batch_size,
+                    nn->batch_outputs[i], nn->g_out, nn->g_in);
+                break;
+        }
+
+        swap_ptr = nn->g_out;
+        nn->g_out = nn->g_in;
+        nn->g_in = swap_ptr;
+    }
+
 }
 
