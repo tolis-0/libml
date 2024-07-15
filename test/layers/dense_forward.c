7ac09ed62eb4ea7865d4a316edeb6aabc1f8113d
tolis-0 <yotravik@gmail.com>
2024-07-15 20:19:41 +0300

    added more test cases for dense_forward

--- test/layers/dense_forward.c
+++ test/layers/dense_forward.c
@@ -29,5 +29,32 @@ int main ()
     1,  ((weight_t[])   {2.0, 3.0, 4.0}),
                         {2.23, 3.53, 4.83});
 
+    dense_forward_test("1x1 without bias", 1, 1,
+        ((value_t[])    {2.0}),
+        ((weight_t[])   {3.0}),
+    0,  NULL,
+                        {6.0});
+
+    dense_forward_test("4x4 without bias", 4, 4,
+        ((value_t[])    {1.0, 2.0, 3.0, 4.0}),
+        ((weight_t[])   {0.1, 0.2, 0.3, 0.4,
+                         0.5, 0.6, 0.7, 0.8,
+                         0.9, 1.0, 1.1, 1.2,
+                         1.3, 1.4, 1.5, 1.6}),
+    0,  NULL,
+                        {3.0, 7.0, 11.0, 15.0});
+
+    dense_forward_test("3x2 random values", 3, 2,
+        ((value_t[])    {1.0, -1.0, 2.0}),
+        ((weight_t[])   {-0.5, 0.3, 0.1, 0.4, -0.2, -0.7}),
+    1,  ((weight_t[])   {-1.0, 2.0}),
+                        {-1.6, 1.2});
+
+    dense_forward_test("2x3 with bias and zero weights", 2, 3,
+        ((value_t[])    {1.0, 1.0}),
+        ((weight_t[])   {0.0, 0.0, 0.0, 0.0, 0.0, 0.0}),
+    1,  ((weight_t[])   {4.7, -1.8, 0.0}),
+                        {4.7, -1.8, 0.0});
+
     return 0;
 }
