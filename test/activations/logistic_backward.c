5ba80a78d001c60fc6ea5438ca7f612daf722bf6
tolis-0 <yotravik@gmail.com>
2024-07-16 21:57:19 +0300

    removed va_args and added assert statements in activations

--- test/activations/logistic_backward.c
+++ test/activations/logistic_backward.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
+#include <assert.h>
 #include "../test.h"
 #include "../../include/nn.h"
 
 
-#define logistic_backward_test(name, N, Y, Gy, ...) \
+#define logistic_backward_test(name, N, X, GY, GX)  \
     do {                                            \
+        assert((N) > 0);                            \
+        assert(__arr_count(X) == (N));              \
+        assert(__arr_count(GY) == (N));             \
+        assert(__arr_count(GX) == (N));             \
+                                                    \
         value_t Gx[N] = __nan_array(N);             \
-        const value_t exp_Gx[N] = __VA_ARGS__;      \
-        logistic_backward(N, Y, Gy, Gx);            \
+                                                    \
+        const value_t *x = X;                       \
+        const grad_t *Gy = GY;                      \
+        const grad_t *exp_Gx = GX;                  \
+                                                    \
+        logistic_backward(N, x, Gy, Gx);            \
         __exp_check_lf(name, N, Gx, 1e-10);         \
     } while (0)
 
@@ -16,38 +26,48 @@ int main ()
 {
     __title("activations/logistic_backward");
 
+
     logistic_backward_test("positive Gy", 4,
         ((value_t[])    {0.668187772168, 0.908877038985, 0.1418510649,   0.5}),
         ((grad_t[])     {0.1, 0.2, 0.3, 0.4}),
-                        {0.022171287329, 0.016563913398, 0.036518802086, 0.1});
+        ((grad_t[])     {0.022171287329, 0.016563913398, 0.036518802086, 0.1}));
+
 
     logistic_backward_test("negative Gy", 4,
         ((value_t[])    {0.2, 0.4, 0.6, 0.8}),
         ((grad_t[])     {-0.1, -0.2, -0.3, -0.4}),
-                        {-0.016, -0.048, -0.072, -0.064});
+        ((grad_t[])     {-0.016, -0.048, -0.072, -0.064}));
+
 
     logistic_backward_test("small Y", 4,
         ((value_t[])    {0.01, 0.001, 0.00017, 0.0000096}),
         ((grad_t[])     {0.1, 0.2, 0.3, 0.4}),
-                        {0.00099, 0.0001998, 0.00005099133, 0.000003839963136});
+        ((grad_t[])     {0.00099, 0.0001998, 0.00005099133, 0.000003839963136}));
+
 
     logistic_backward_test("big Y", 4,
         ((value_t[])    {0.99, 0.999, 0.99983, 0.9999904}),
         ((grad_t[])     {0.1, 0.2, 0.3, 0.4}),
-                        {0.00099, 0.0001998, 0.00005099133, 0.000003839963136});
+        ((grad_t[])     {0.00099, 0.0001998, 0.00005099133, 0.000003839963136}));
+
 
     logistic_backward_test("zero Gy", 6,
         ((value_t[])    {0.99998475, 0.3512384, 1.0, 0.0, 0.0001675, 0.5}),
         ((grad_t[])     {0.0, 0.0, 0.0, 0.0, 0.0, 0.0}),
-                        {0.0, 0.0, 0.0, 0.0, 0.0, 0.0});
+        ((grad_t[])     {0.0, 0.0, 0.0, 0.0, 0.0, 0.0}));
+
 
     logistic_backward_test("zero Y", 6,
         ((value_t[])    {0.0, 0.0, 0.0, 0.0, 0.0, 0.0}),
         ((grad_t[])     {0.1, 0.2, 0.3, -0.3, -0.2, -0.1}),
-                        {0.0, 0.0, 0.0,  0.0,  0.0,  0.0});
+        ((grad_t[])     {0.0, 0.0, 0.0,  0.0,  0.0,  0.0}));
+
 
     logistic_backward_test("one Y", 6,
         ((value_t[])    {1.0, 1.0, 1.0, 1.0, 1.0, 1.0}),
         ((grad_t[])     {0.1, 0.2, 0.3, -0.3, -0.2, -0.1}),
-                        {0.0, 0.0, 0.0,  0.0,  0.0,  0.0});
+        ((grad_t[])     {0.0, 0.0, 0.0,  0.0,  0.0,  0.0}));
+
+
+    return 0;
 }
--- test/activations/relu_forward.c
+++ test/activations/relu_forward.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
+#include <assert.h>
 #include "../test.h"
 #include "../../include/nn.h"
 
 
-#define relu_forward_test(name, N, X, ...)      \
+#define relu_forward_test(name, N, X, Y)        \
     do {                                        \
+        assert((N) > 0);                        \
+        assert(__arr_count(X) == (N));          \
+        assert(__arr_count(Y) == (N));          \
+                                                \
         value_t y[N] = __nan_array(N);          \
-        const value_t exp_y[N] = __VA_ARGS__;   \
-        relu_forward(N, X, y);                  \
+                                                \
+        const value_t *x = X;                   \
+        const value_t *exp_y = Y;               \
+                                                \
+        relu_forward(N, x, y);                  \
         __exp_check_lf(name, N, y, 1e-50);      \
     } while (0)
 
@@ -16,33 +24,41 @@ int main ()
 {
     __title("activations/relu_forward");
 
+
     relu_forward_test("random example", 10,
         ((value_t[])    {0.7, 2.3, -1.8, 0.0, 0.1, -0.1, -2.7, 4.0, -3.0, -0.0}),
-                        {0.7, 2.3,  0.0, 0.0, 0.1,  0.0,  0.0, 4.0,  0.0,  0.0});
+        ((value_t[])    {0.7, 2.3,  0.0, 0.0, 0.1,  0.0,  0.0, 4.0,  0.0,  0.0}));
+
 
     relu_forward_test("positive values", 6,
         ((value_t[])    {1.0, 2.0, 3.0, 4.0, 5.0, 10.0}),
-                        {1.0, 2.0, 3.0, 4.0, 5.0, 10.0});
+        ((value_t[])    {1.0, 2.0, 3.0, 4.0, 5.0, 10.0}));
+
 
     relu_forward_test("negative values", 6,
         ((value_t[])    {-1.0, -2.0, -3.0, -4.0, -5.0, -10.0}),
-                        {0.0,   0.0,  0.0,  0.0,  0.0,   0.0});
+        ((value_t[])    {0.0,   0.0,  0.0,  0.0,  0.0,   0.0}));
+
 
     relu_forward_test("mixed values", 6,
         ((value_t[])    {-1.0, 2.0, -3.0, 4.0, -5.0, 6.0}),
-                        {0.0,  2.0,  0.0, 4.0,  0.0, 6.0});
+        ((value_t[])    {0.0,  2.0,  0.0, 4.0,  0.0, 6.0}));
+
 
     relu_forward_test("zero values", 4,
         ((value_t[])    {0.0, 0.0, 0.0, 0.0}),
-                        {0.0, 0.0, 0.0, 0.0});
+        ((value_t[])    {0.0, 0.0, 0.0, 0.0}));
+
 
     relu_forward_test("large values", 8,
         ((value_t[])    {2e5, -2e5, 7e10, -7e10, 3e20, -3e20, 5e40, -5e40}),
-                        {2e5,  0.0, 7e10,   0.0, 3e20,   0.0, 5e40,   0.0});
+        ((value_t[])    {2e5,  0.0, 7e10,   0.0, 3e20,   0.0, 5e40,   0.0}));
+
 
     relu_forward_test("small values", 8,
         ((value_t[])    {1e-6, -1e-6, 2e-9, -2e-9, 3e-19, -3e-19, 4e-39, -4e-39}),
-                        {1e-6,   0.0, 2e-9,   0.0, 3e-19,    0.0, 4e-39,    0.0});
+        ((value_t[])    {1e-6,   0.0, 2e-9,   0.0, 3e-19,    0.0, 4e-39,    0.0}));
+
 
     return 0;
 }
