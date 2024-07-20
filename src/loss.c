5d22ddfaa5bfb010b8c45a4bb9066716f8c04f09
tolis-0 <yotravik@gmail.com>
2024-07-20 16:15:11 +0300

    function to calculate mse

--- src/loss.c
+++ src/loss.c
@@ -2,9 +2,23 @@
 #include "../include/nn.h"
 
 
-/*	Implements grad = y - t */
+/*  Implements grad = y - t */
 void loss_diff_grad(int d, const value_t *y, const value_t *t, value_t *grad)
 {
-	cblas_dcopy(d, y, 1, grad, 1);
+    cblas_dcopy(d, y, 1, grad, 1);
     cblas_daxpy(d, -1.0, t, 1, grad, 1);
+}
+
+
+/*  Calculates mean squared error */
+value_t loss_mse(int n, const value_t *y, const value_t *t)
+{
+    value_t diff, mse = 0.0;
+
+    for (int i = 0; i < n; i++) {
+        diff = y[i] - t[i];
+        mse += diff * diff;
+    }
+
+    return mse / n;
 }
